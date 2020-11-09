84fca97950d155626b994f99e02a28f0ea6543fd
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-11-08 22:40:41 -0500

    added user authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middelware/errorMiddleware.js";
 const PORT = process.env.PORT || 5000;
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Custom error handling middleware for routes that don't exist
 app.use(notFound);
