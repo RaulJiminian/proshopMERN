29b77e47959e2dea12edf5d64a8b5e4f0dec2808
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-11-08 19:38:20 -0500

    Added Shopping Cart reducer action screen

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Alert } from "react-bootstrap";
 
 const Message = ({ variant, children }) => {
-  return <Alert varient={variant}>{children}</Alert>;
+  return <Alert variant={variant}>{children}</Alert>;
 };
 
 Message.defaultProps = {
