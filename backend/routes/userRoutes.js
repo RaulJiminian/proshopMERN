137ef0683071cf5883a976c487e61f8c7b53bc51
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-11-09 01:35:53 -0500

    backend user authentication registration and password encryption completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middelware/authMiddleware.js";
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
