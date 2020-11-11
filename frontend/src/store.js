2ad5ef6460144673bc2abe8687335e3cc0473fd7
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-11-11 16:48:35 -0500

    update profile endpoint

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,7 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
@@ -14,6 +14,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
