2a0ef46e27d46086cfae95b022e9fc47906fd4e2
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-11-15 20:45:16 -0500

    order pay reducer and action on frontend; updated paid endpoint on backend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from "./reducers/orderReducers";
 import { cartReducer } from "./reducers/cartReducers";
 
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
